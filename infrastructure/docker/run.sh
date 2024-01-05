f9931a19c21bd730132c6b6e90b0897e9c2db150
Jesper Kristensen <cableman@linuxdev.dk>
2024-01-05 14:56:38 +0100

    ITKDev: change docker image location

--- infrastructure/docker/run.sh
+++ infrastructure/docker/run.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+# Exit when any command fails
+set -e
+set -x
+
 APP_VERSION=develop
 VERSION=latest
 
-docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=danskernesdigitalebibliotek/follow-searches:${VERSION} --file="follow-searches/Dockerfile" follow-searches
-docker build --no-cache --build-arg VERSION=${VERSION} --tag=danskernesdigitalebibliotek/follow-searches-nginx:${VERSION} --file="nginx/Dockerfile" nginx
+docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=itkdev/follow-searches:${VERSION} --file="follow-searches/Dockerfile" follow-searches
+docker push itkdev/follow-searches:${VERSION}
 
-docker push danskernesdigitalebibliotek/follow-searches:${VERSION}
-docker push danskernesdigitalebibliotek/follow-searches-nginx:${VERSION}
+docker build --no-cache --build-arg VERSION=${VERSION} --tag=itkdev/follow-searches-nginx:${VERSION} --file="nginx/Dockerfile" nginx
+docker push itkdev/follow-searches-nginx:${VERSION}
